53e3cf804157d7b2d196d9d41f82a72a56b540a9
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-11-14 10:58:36 -0800

    feat: export TSA timestamp verification functions and types
    Export TSA timestamp verification functions and related types from
    the main entry point:
    - verifyRFC3161Timestamp: Verify timestamps against authorities
    - verifyBundleTimestamp: Extract and verify bundle timestamps
    - TimestampVerificationData: Bundle timestamp data type
    - RFC3161Timestamp: Timestamp data type
    - RawTimestampAuthority: Timestamp authority type
    
    This allows external consumers to use TSA verification functionality
    independently of the main SigstoreVerifier class.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SigstoreVerifier } from "./sigstore.js";
-export type { SigstoreBundle, TLogEntry, VerificationMaterial, MessageSignature } from "./bundle.js";
-export type { TrustedRoot, Sigstore } from "./interfaces.js";
+export type { SigstoreBundle, TLogEntry, VerificationMaterial, MessageSignature, TimestampVerificationData, RFC3161Timestamp } from "./bundle.js";
+export type { TrustedRoot, Sigstore, RawTimestampAuthority } from "./interfaces.js";
+export { verifyRFC3161Timestamp, verifyBundleTimestamp } from "./timestamp/tsa.js";
